f6209d758e7917be4358f4b531e05c8cce90d199
Your Name <you@example.com>
2014-11-02 00:43:02 +0200

    пофиксил детект tun девайсаё

--- 10vpnroutes.sh
+++ 10vpnroutes.sh
@@ -1,19 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-if `ip route list | grep default | grep -v tun`; then
-	# default gateway is not on tun device
+set -x
 
-	GATEWAY_DEFAULT=$(ip route list | sed -n -e "s/^default.*[[:space:]]\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\).*/\1/p")
+# срабатывает и на tun интерфейс
+IFACE="$1"
 
-	for cfg in `ls /etc/openvpn/*.conf`; do
-		for ip in `cat $cfg | grep remote | grep -v '#' | awk '{print $2}'`; do
-			route del $ip
-			route add $ip gw $GATEWAY_DEFAULT
-		done
-	done
-
-	route del default
-	/etc/init.d/openvpn restart
-else
-	echo 'default gateway is already on vpn'
+if [[ $IFACE == *tun* ]]; then
+	echo 'tun iface'
+	echo $IFACE
+	exit 0
 fi
+
+DG_IFACE=`ip route list | grep 'default' | awk '{print $5}'`
+
+if [[ $DG_IFACE == *tun* ]]; then
+	echo 'default gateway is already on vpn'
+	exit 0
+fi
+
+GATEWAY_DEFAULT=$(ip route list | sed -n -e "s/^default.*[[:space:]]\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\).*/\1/p")
+
+for cfg in `ls /etc/openvpn/*.conf`; do
+	for ip in `cat $cfg | grep remote | grep -v '#' | awk '{print $2}'`; do
+		route del $ip
+		route add $ip gw $GATEWAY_DEFAULT
+	done
+done
+
+route del default
+/etc/init.d/openvpn restart
